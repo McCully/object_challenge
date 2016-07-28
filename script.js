13cb326b49884b0d5d7cac78ac90ee9620f7956d
mat mccully <mccully1894@gmail.com>
2016-07-28 09:48:00 -0500

    objects and functions created

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+
 var locations = []; // this array will hold your objects
 
-// object constructor
+function Regions ( state, revenue, locationCount){  // object constructor
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+
+  this.averageRevenue = function() {              // function to get avg revenue
+    return (this.revenue / this.locationCount);
+  }
+};
+
+var illinois = new Regions("IL", 5000 , 12);
+var minnesota = new Regions("MN", 300, 3);          // notation
+var nevada = new Regions("NV", 25000, 1);
+
+locations.push(illinois, minnesota, nevada);// object constructor
 
 // object instances
 
